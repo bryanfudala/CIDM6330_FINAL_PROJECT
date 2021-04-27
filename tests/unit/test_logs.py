bb6e17a2f2a31a5dba36bc484d24a663f65b72ab
bryanfudala <btfudala1@buffs.wtamu.edu>
2021-04-27 01:04:52 -0700

    unit test

--- tests/unit/test_logs.py
+++ tests/unit/test_logs.py
@@ -32,14 +32,28 @@ def test_adding_record_to_dailylog(session):
     assert list(rows) == [(1, 20, "spinachrolls", 100,10)]
 
 
-def test_append_dailylog(session):
-    x = models.DailyLog(1, 20, "spinachrolls", 100,10)
+def test_computation_of_calories(session):
+    x = models.Meals(1, "protein bar", 20, 20, 9, 250)
 
     x_repository = repository.SqlAlchemyRepository(session)
     x_repository.add(x)
     session.commit()
-
-    rows = session.execute(
-        'SELECT ID , IDMeal, MealName, calories, protein FROM "dailylog"'
+    
+    y = session.execute(
+        'SELECT Calories  FROM "Meals" WHERE ID=1'
     )
-    assert list(rows) == [(1, 20, "spinachrolls", 100,10)]
+
+    assert y == 250
+
+def test_biometrics_update(session):
+    x = models.Bio(190)
+
+    x_repository = repository.SqlAlchemyRepository(session)
+    x_repository.edit(x)
+    session.commit()
+    
+    y = session.execute(
+        'SET WEIGHT = 190  FROM "Meals" WHERE ID=1'
+    )
+
+    assert y == 190
